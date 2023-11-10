748f600868d95f83abb3332b820105ccedd85adb
John Doe <andridaniata@gmail.com>
2023-11-10 22:59:23 +0700

    mengambil data

---
+++ get-data.sql
@@ -0,0 +1,12 @@
+-- Mengambil data dari tabel Products
+SELECT * FROM Products LIMIT 10;
+
+
+-- Mengambil data dari tabel Categories
+SELECT * FROM Categories LIMIT 7;
+
+-- Mengambil data dari tabel Orders
+SELECT * FROM Orders LIMIT 9;
+
+-- Mengambil data dari tabel OrderDetails
+SELECT * FROM OrderDetails LIMIT 6;
